d0f5c2e668a9544b067d70dc5359be6e3caf4236
Michael Ballif <mballifcodes@gmail.com>
2022-12-21 23:30:58 -0700

    create base functions to accomplish user stories
    Add functions for arithmetic: add(), subtract(), divide(), multiply();
    Create arrays to capture numbers client enters in calculator;

--- index.js
+++ index.js
@@ -1,2 +1,69 @@
+/// Calculator
+// US1 - Arithmetic Functions:
+function add(num1, num2) {
+    return num1+num2;
+};
+
+function subtract(num1, num2) {
+    return num1-num2;
+};
+
+function multiply(num1, num2) {
+    return num1*num2;
+};
+
+function divide(num1, num2) {
+    return num1/num2;
+};
+
+// US2 - Operate Function
+function operate (num1, num2, func) {
+    switch (func){
+        case add:
+            return add(num1, num2);
+            break;
+        case subtract:
+            return subtract(num1, num2);
+            break;
+        case multiply:
+            return multiply(num1, num2);
+            break;
+        case divide:
+            return divide (num1, num2);
+            break;
+    }
+}
+
+// US4 - popDisplay()
+function popDisplay(char){
+    if (char == '+' || char == '-' || char == '/' || char == '*') {
+        display.textContent = display.textContent + ` ${char} `;
+    } else {
+        display.textContent = display.textContent + `${char}`;
+    }
+}
+
 const display = document.querySelector('.display');
-console.log(display);
+let numberOrder = [];
+let operatorOrder = [];
+let currentNumber='';
+
+const operands = Array.from(document.querySelectorAll('.operand'));
+for (let i = 0; i < operands.length; i++){
+    operands[i].addEventListener('click', () =>{
+        currentNumber = currentNumber + `${operands[i].textContent}`;
+        display.textContent = display.textContent + `${operands[i].textContent}`;
+    })
+}
+const operators = Array.from(document.querySelectorAll('.operator'));
+for (let i = 0; i < operators.length; i++){
+    operators[i].addEventListener('click', () =>{
+        if (numberOrder[0]) {
+                numberOrder.push(currentNumber);
+                display.textContent
+
+            } else {
+                // do nothing
+            }
+    })
+}
